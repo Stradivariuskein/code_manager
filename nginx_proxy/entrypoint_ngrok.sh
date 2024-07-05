af1006be18ed26a5a94e90b164b5b4cbdb5c6083
Alan Luque <you@example.com>
2024-07-05 22:30:51 +0000

    testing pass

--- nginx_proxy/entrypoint_ngrok.sh
+++ nginx_proxy/entrypoint_ngrok.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Archivo temporal para almacenar la salida de ngrok
-
+echo ${IP_TO_HOST}
 # Ejecuta ngrok en segundo plano y redirige la salida al archivo temporal
-ngrok http 172.22.0.2:443 2>&1 1>/tmp/ngrok.log &
+ngrok http ${IP_TO_HOST}:443 2>&1 1>/tmp/ngrok.log &
 
 
 sleep 10
